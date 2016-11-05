895d15176c02bdc55d2ba97a95b897af6fc75d24
Antonio Martinelli <antonio.martinelli93@gmail.com>
2016-11-05 19:08:43 +0100

    completed notebook with results and main func + refactor workshop_04.py

--- 2016-11-04/workshop_04.py
+++ 2016-11-04/workshop_04.py
@@ -1,28 +1,38 @@
 from pyplasm import *
 import numpy
 
-def normalize_list(verts):
-	for j in range(len(verts)):
-		for i in range(len(verts[j])):
-			if(abs(verts[j][i]) < 0.001):
-				verts[j][i] = 0
+def round_vertices(verts):
+	"""This function round the coordinates of a given list of vertices: if the vertex has a coordinate 
+		smaller than 0.001 it will be rounded to 0, alternatively it will be rounded to the first decimal.
+	"""
+	for i in range(len(verts)):
+		for j in range(len(verts[i])):
+			if(abs(verts[i][j]) < 0.001):
+				verts[i][j] = 0
 			else:
-				verts[j][i] = round(verts[j][i],1)
+				verts[i][j] = round(verts[i][j],1)
 
-def makeDict(listUkpol):
+def compute_incident_faces(listUkpol):
+	"""This function takes in input the result of UKPOL() function and 
+		returns a dictionary that has keyvalue as the coordinates of a vertex and 
+		as value a list of the cells incident in that point
+	"""
 	dictionary = {}
 	verts = listUkpol[0]
-	normalize_list(verts)
+	round_vertices(verts)
 	cells = listUkpol[1]
 	for cell in cells:
-		for label in cell:
-			point = str(verts[int(label)-1])
+		for elem in cell:
+			point = str(verts[int(elem)-1])
 			if(point not in dictionary):
 				dictionary[point] = []
-			dictionary[point].append(label)
+			dictionary[point].append(elem)
 	return dictionary
 
 def check_complanarity(verts, cells):
+	"""This function takes a list of vertices and a list of cells
+	   in input and return True if all the cells are complanar, False elsewhere"""
+	round_vertices(verts)
 	for cell in cells:
 		if(len(cell) > 3):
 			matrix = []
@@ -39,12 +49,14 @@ def check_complanarity(verts, cells):
 				return False
 	return True
 
-def cleanCells(cells, verts):
+def clean_cells(cells, verts):
+	"""This function takes in input a list of vertices and a list of cells and returns the cells from the former list that doesn't remains
+	   with coordinate z = 0"""
 	cleaned = []
 	for i in range(len(cells)-1):
 		isBaseCell = True
 		for pointIndex in cells[i]:
-			print "punto" + str(pointIndex)
+			print "point: " + str(pointIndex)
 			if(verts[pointIndex-1][2] != 0):
 				isBaseCell = False
 		if(not isBaseCell):
@@ -52,21 +64,33 @@ def cleanCells(cells, verts):
 	return cleaned
 
 
+def ggpl_L_and_U_roof_builder(verts, cells):
+	"""This function takes in input a list of vertices and a list of
+		cells and returns an HPC model of an L/U roof and its frame."""
+	frameOffset = 0.1
+	if(not check_complanarity(verts, cells)):
+		return None
+	roofModel = MKPOL([verts,cells, None])
+	cleanedCells = clean_cells(cells,verts)
 
-verts = [[0,0,0],[0,3,0],[6,3,0],[6,9,0],[9,9,0],[9,0,0],[1.5,1.5,2],[7.5,1.5,2],[7.5,7.5,2]]
-cells = [[1,7,2],[2,7,8,3],[3,8,9,4],[4,9,5],[8,6,5,9],[1,6,8,7],[1,6,3,2],[3,6,5,4]]
-verts = [[0,0,0], [0,10,0],[6,10,0],[6,8,0],[3,8,0],[3,4,0],[6,4,0],[6,0,0],[5,2,2],[1,2,2],[1,9,2],[5,9,2]]
-cells = [[9,8,7],[7,6,10,9],[9,10,1,8],[1,10,11,2],[2,11,12,3],[3,12,4],[4,12,11,5],[5,11,10,6],[8,7,6,1],[1,6,5,2],[2,5,4,3]]
-#verts = [[0,0,0],[6,0,0],[6,-12,0],[3,-12,0],[3,-3,0],[0,-3,0],[1.5,-1.5,3],[4.5,-1.5,3],[4.5,-10.5,3]]
-#cells = [[1,7,6],[2,8,7,1],[2,3,9,8],[4,3,9],[4,9,8,5],[5,8,7,6],[6,5,2,1],[5,4,3,2]]
-frameOffset = 0.1
-tetto = MKPOL([verts,cells, None])
-cells = cleanCells(cells,verts)
-tetto_sopra = MKPOL([verts,cells,None])
-tetto_sopra = OFFSET([frameOffset,frameOffset,frameOffset])(tetto_sopra)
-tetto_sopra = T([3])([frameOffset])(tetto_sopra)
-tetto_sopra = COLOR(Color4f([1/255., 61/255., 31/255.,1]))(tetto_sopra)
-travi = OFFSET([.1,.1,.1])(SKEL_1(tetto))
-travi = S([3])(.95)(travi)
-travi = COLOR(Color4f([132/255., 54/255., 9/255.,1]))(travi)
-VIEW((STRUCT([travi, tetto_sopra])))
+	roof = MKPOL([verts, cleanedCells, None])
+	roof = OFFSET([frameOffset, frameOffset, frameOffset])(roof)
+	roof = T([3])([frameOffset])(roof)
+	roof = COLOR(Color4f([68/255., 85/255., 51/255.,1]))(roof)
+
+	roofFrame = OFFSET([.1,.1,.1])(SKEL_1(roofModel))
+	roofFrame = S([3])(.95)(roofFrame)
+	roofFrame = COLOR(Color4f([48/255., 28/255., 24/255.,1]))(roofFrame)
+
+	return STRUCT([roof,roofFrame])
+
+#first execution
+verts1 = [[0,0,0],[9,0,0],[9,7,0],[6,7,0],[6,4,0],[0,4,0],[2,2,2],[7.5,2,2],[7.5,5.5,2]]
+cells1 = [[1,7,6],[1,2,8,7],[2,3,9,8],[3,9,4],[4,9,8,5],[5,8,7,6],[6,5,2,1],[2,5,4,3]]
+
+#second execution
+verts2 = [[0,0,0],[3,0,0],[3,6,0],[8,6,0],[8,4,0],[10,4,0],[10,9,0],[0,9,0],[1.5,1.5,2],[1.5,7.5,2],[9,7.5,2],[9,5,2]]
+cells2 = [[1,2,9],[2,3,10,9],[3,4,11,10],[4,5,12,11],[5,6,12],[6,7,11,12],[7,8,10,11],[8,1,9,10],[1,2,3,8],[3,4,7,8],[4,5,6,7]]
+
+VIEW(ggpl_L_and_U_roof_builder(verts1, cells1))
+VIEW(ggpl_L_and_U_roof_builder(verts2, cells2))
